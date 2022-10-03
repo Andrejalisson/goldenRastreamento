f346dcb096991ead6830d71a2064a48ddc64b081
André Jálisson <andrejalisson@gmail.com>
2022-10-03 13:29:22 -0300

    Rastreamento

--- resources/views/site/rastreamento.blade.php
+++ resources/views/site/rastreamento.blade.php
@@ -21,47 +21,71 @@
                 <div class="content-block my-auto">
                     <div class="title-block">
                         <span class="tag-line">#VemPraGolden</span>
-                        <h2>+ de 10 anos de Experiência</h2>
+                        <h2>Rastreamento da Golden Service</h2>
                     </div><!-- /.title-block -->
-                    <p>Segurança, confiabilidade e atendimento personalizado são os diferenciais que norteiam o nosso trabalho. Somos sua melhor escolha quando o assunto for segurança.</p><br>
-                    <p>Há mais de 10 anos garantimos segurança com confiabilidade. Com foco na qualidade do atendimento, contribuímos para o aumento da qualidade de vida. Com atitude inovadora, postura ética e transparente em tudo o que fazemos. Estamos prontos para atender as necessidades do mercado, gerando economia e satisfação. Golden Service, sua vida mais segura.</p>
-                    <a href="#" class="more-btn">Solicitar Orçamento</a>
+                    <p>Rastreamento da Golden Service, mais segurança para você e proteção para o seu veículo. Com o rastreamento você poderá saber a localização do seu veículo em qualquer hora do dia, e, em caso de roubo ou furto facilita a localização e a recuperação</p><br>
                 </div><!-- /.content-block -->
             </div><!-- /.col-lg-6 -->
         </div><!-- /.row -->
     </div><!-- /.container -->
 </section><!-- /.about-style-two -->
-<section class="mission-style-one wow fadeInUp home-page-two" data-wow-duration="1300ms">
+<section class="pricing-style-one pricing-page">
     <div class="container">
-        <div class="inner-container">
-            <div class="single-mission-one">
-                <div class="count-block">
-                    01
-                </div><!-- /.count-block -->
-                <h3>Missão</h3>
-                <p>Melhorar a qualidade de vida dos nossos <br>clientes com soluções customizadas em segurança.</p>
-            </div><!-- /.single-mission-one -->
-            <div class="single-mission-one">
-                <div class="count-block">
-                    02
-                </div><!-- /.count-block -->
-                <h3>Visão</h3>
-                <p>Crescer de forma responsável, buscando a <br> melhoria continua atendendo diversos setores do mercado.</p>
-            </div><!-- /.single-mission-one -->
-            <div class="single-mission-one">
-                <div class="count-block">
-                    03
-                </div><!-- /.count-block -->
-                <h3>Valores</h3>
-                <p><strong>Sinceridade e Confiança</strong> <br> Somos transparentes, trabalhamos com a verdade; <br>
-                <strong>Meritocracia e autorresponsabilidade</strong> <br> Você é os seus resultados <br>
-                <strong>Gratidão e Felicidade</strong> <br> Ser grato por tudo e felizes na jornada, sempre. <br>
-                <strong>Garra e Consistência</strong> <br> O que precisa ser feito, será feito. <br>
-                </p>
-            </div>
-        </div><!-- /.inner-container -->
+        <div class="title-block text-center ">
+            <span class="tag-line">Planning</span><!-- /.tag-line -->
+            <h2>Pricing Table</h2>
+            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt <br> mollit anim id est laborum.</p>
+        </div><!-- /.title-block -->
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="single-pricing-style-one text-center popular-pricing wow fadeInUp" data-wow-duration="1300ms" data-wow-delay="500ms" >
+                    <div class="top-block">
+                        <p>Plano TOP</p>
+                        <h3>TOP</h3>
+                        <span class="price">R$70,00<small>(carro)</small></span><br>
+                        <span class="price">R$65,00<small>(Moto)</small></span>
+                    </div><!-- /.top-block -->
+                    <div class="bottom-block">
+                        <ul class="feature-list">
+                            <li><i class="fa fa-check"></i>Rastreamento e monitoramento 24h com acompanhamento através do site e aplicativo;</li>
+                            <li><i class="fa fa-check"></i>Sirene sonora de segurança para moto;</li>
+                            <li><i class="fa fa-check"></i>Canal 24h exclusivo 0800 para comunicar sinistro (roubo/furto)</li>
+                            <li><i class="fa fa-check"></i>Equipe de pronta resposta (busca e apoio) para auxiliar na recuperação do veículo;</li>
+                            <li><i class="fa fa-check"></i>Sensores de ignição e cerca;</li>
+                            <li><i class="fa fa-check"></i>Função bloqueio e desbloqueio (exclusivo pela central</li>
+                            <li><i class="fa fa-check"></i>Central de monitoramento 24h para avisar sempre que haver desconexão de bateria.</li>
+                        </ul><!-- /.feature-list -->
+                        <div class="button-block">
+                            <a target="_blank" href="https://wa.me/5585985214611" class="thm-btn">Contratar</a>
+                        </div><!-- /.button-block -->
+                    </div><!-- /.bottom-block -->
+                </div><!-- /.single-pricing-style-one -->
+            </div><!-- /.col-lg-6 -->
+            <div class="col-lg-6">
+                <div class="single-pricing-style-one text-center  wow fadeInUp" data-wow-duration="1300ms" data-wow-delay="0ms">
+                    <div class="top-block">
+                        <p>Plano Econômico</p>
+                        <h3>Econômico</h3>
+                        <span class="price">R$55,00<small>(carro)</small></span><br>
+                        <span class="price">R$50,00<small>(Moto)</small></span>
+                    </div><!-- /.top-block -->
+                    <div class="bottom-block">
+                        <ul class="feature-list">
+                            <li><i class="fa fa-check"></i>Bloqueie e desbloqueie pelo aplicativo</li>
+                            <li><i class="fa fa-check"></i>Monitore pelo site</li>
+                            <li><i class="fa fa-check"></i>Ative alertas de antifurto</li>
+                            <li><i class="fa fa-check"></i>Acione sirene sonora (exclusivo para motos)</li>
+                            <li><i class="fa fa-check"></i>Consulta de trajetos</li>
+                        </ul><!-- /.feature-list -->
+                        <div class="button-block">
+                            <a target="_blank" href="https://wa.me/5585985214611" class="thm-btn">Contratar</a>
+                        </div><!-- /.button-block -->
+                    </div><!-- /.bottom-block -->
+                </div><!-- /.single-pricing-style-one -->
+            </div><!-- /.col-lg-6 -->
+        </div><!-- /.row -->
     </div><!-- /.container -->
-</section><!-- /.mission-style-one -->
+</section><!-- /.pricing-style-one -->
 @endsection
 
 
