691e9e9974f7e52f4bc52b5f3d71f413e767842c
André Jálisson <andrejalisson@gmail.com>
2022-10-06 17:09:57 -0300

    Novas sessions página.

--- resources/views/site/bateria.blade.php
+++ resources/views/site/bateria.blade.php
@@ -14,54 +14,93 @@
         <div class="row">
             <div class="col-lg-6">
                 <div class="image-block">
-                    <img src="/images/resources/video-1-1.png" alt="Awesome Image" />
+                    <img src="/images/resources/rastreamento1.jpg" style="max-width: 100%" alt="Awesome Image" />
                 </div>
             </div>
             <div class="col-lg-6 d-flex">
                 <div class="content-block my-auto">
                     <div class="title-block">
                         <span class="tag-line">#VemPraGolden</span>
-                        <h2>+ de 10 anos de Experiência</h2>
+                        <h2>Rastreamento Veicular</h2>
                     </div><!-- /.title-block -->
-                    <p>Segurança, confiabilidade e atendimento personalizado são os diferenciais que norteiam o nosso trabalho. Somos sua melhor escolha quando o assunto for segurança.</p><br>
-                    <p>Há mais de 10 anos garantimos segurança com confiabilidade. Com foco na qualidade do atendimento, contribuímos para o aumento da qualidade de vida. Com atitude inovadora, postura ética e transparente em tudo o que fazemos. Estamos prontos para atender as necessidades do mercado, gerando economia e satisfação. Golden Service, sua vida mais segura.</p>
-                    <a href="#" class="more-btn">Solicitar Orçamento</a>
+                    <p>Rastreamento da Golden Service, mais segurança para você e proteção para o seu veículo. Com o rastreamento você poderá saber a localização do seu veículo em qualquer hora do dia, e, em caso de roubo ou furto facilita a localização e a recuperação</p><br>
                 </div><!-- /.content-block -->
             </div><!-- /.col-lg-6 -->
         </div><!-- /.row -->
     </div><!-- /.container -->
 </section><!-- /.about-style-two -->
-<section class="mission-style-one wow fadeInUp home-page-two" data-wow-duration="1300ms">
+<section class="offer-style-one">
     <div class="container">
-        <div class="inner-container">
-            <div class="single-mission-one">
-                <div class="count-block">
-                    01
-                </div><!-- /.count-block -->
-                <h3>Missão</h3>
-                <p>Melhorar a qualidade de vida dos nossos <br>clientes com soluções customizadas em segurança.</p>
-            </div><!-- /.single-mission-one -->
-            <div class="single-mission-one">
-                <div class="count-block">
-                    02
-                </div><!-- /.count-block -->
-                <h3>Visão</h3>
-                <p>Crescer de forma responsável, buscando a <br> melhoria continua atendendo diversos setores do mercado.</p>
-            </div><!-- /.single-mission-one -->
-            <div class="single-mission-one">
-                <div class="count-block">
-                    03
-                </div><!-- /.count-block -->
-                <h3>Valores</h3>
-                <p><strong>Sinceridade e Confiança</strong> <br> Somos transparentes, trabalhamos com a verdade; <br>
-                <strong>Meritocracia e autorresponsabilidade</strong> <br> Você é os seus resultados <br>
-                <strong>Gratidão e Felicidade</strong> <br> Ser grato por tudo e felizes na jornada, sempre. <br>
-                <strong>Garra e Consistência</strong> <br> O que precisa ser feito, será feito. <br>
-                </p>
-            </div>
-        </div><!-- /.inner-container -->
+        <div class="title-block">
+            <h2>Por que escolher a Bidon Golden Service</h2>
+            <span>Somos franqueado da Bidon Corretora de Seguros autorizada pela SUSEP, e por meio dela, temos parceria com mais de 30 seguradoras do país! Permitindo encontrarmos um seguro para seu veículo que atenda as suas necessidades e ao seu bolso!</span><!-- /.tag-line -->
+
+        </div><!-- /.title-block -->
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="single-offer-style-one wow fadeInUp" data-wow-duration="1300ms" data-wow-delay="0ms">
+                    <div class="icon-block">
+                        <i class="fa fa-calculator"></i>
+                    </div><!-- /.icon-block -->
+                    <h3><a href="#">Preços  <br> Imbatíveis</a></h3>
+                    <p>Com a nossa excelente gama de seguradoras e acordos diferenciados, conseguimos preços imbatíveis para você.</p>
+                    <a href="#" class="more-link">Contratar</a>
+                </div><!-- /.single-offer-style-one -->
+            </div><!-- /.col-lg-3 -->
+            <div class="col-lg-4 col-md-6">
+                <div class="single-offer-style-one wow fadeInUp" data-wow-duration="1300ms" data-wow-delay="500ms">
+                    <div class="icon-block">
+                        <i class="fa fa-headphones"></i>
+                    </div><!-- /.icon-block -->
+                    <h3><a href="#">Atendimento <br> Personalizado</a></h3>
+                    <p>A nossa equipe está pronta para te atender e instruir sempre que você precisar.</p>
+                    <a href="#" class="more-link">Contratar</a>
+                </div><!-- /.single-offer-style-one -->
+            </div><!-- /.col-lg-3 -->
+            <div class="col-lg-4 col-md-6">
+                <div class="single-offer-style-one wow fadeInUp" data-wow-duration="1300ms" data-wow-delay="1000ms">
+                    <div class="icon-block">
+                        <i class="fa fa-users"></i>
+                    </div><!-- /.icon-block -->
+                    <h3><a href="#">Milhares de <br> Clientes</a></h3>
+                    <p>Nada melhor do que escolher um serviço que já é aprovado por muitos consumidores.</p>
+                    <a href="#" class="more-link">Contratar</a>
+                </div><!-- /.single-offer-style-one -->
+            </div><!-- /.col-lg-3 -->
+
+            <div class="col-lg-4 col-md-6">
+                <div class="single-offer-style-one wow fadeInUp" data-wow-duration="1300ms" data-wow-delay="0ms">
+                    <div class="icon-block">
+                        <i class="fa fa-trophy"></i>
+                    </div><!-- /.icon-block -->
+                    <h3><a href="#">Estamos presente <br> em todo o País</a></h3>
+                    <p>Os nossos consultores estão espalhados por todo o território nacional, prontos para te atender.</p>
+                    <a href="#" class="more-link">Contratar</a>
+                </div><!-- /.single-offer-style-one -->
+            </div><!-- /.col-lg-3 -->
+            <div class="col-lg-4 col-md-6">
+                <div class="single-offer-style-one wow fadeInUp" data-wow-duration="1300ms" data-wow-delay="500ms">
+                    <div class="icon-block">
+                        <i class="fa fa-handshake-o"></i>
+                    </div><!-- /.icon-block -->
+                    <h3><a href="#">Seguradoras</a></h3>
+                    <p>Parceria com as melhores seguradoras do mercado. Trabalhamos com as melhores para oferecer o que há de melhor no mercado aos nossos amigos clientes.</p>
+                    <a href="#" class="more-link">Contratar</a>
+                </div><!-- /.single-offer-style-one -->
+            </div><!-- /.col-lg-3 -->
+            <div class="col-lg-4 col-md-6">
+                <div class="single-offer-style-one wow fadeInUp" data-wow-duration="1300ms" data-wow-delay="1000ms">
+                    <div class="icon-block">
+                        <i class="fa fa-mobile"></i>
+                    </div><!-- /.icon-block -->
+                    <h3><a href="#">Tecnologia</a></h3>
+                    <p>Tecnologia de última geração para trazer o que há de novo, contemporâneo e usual no mercado de seguros. Trabalhamos para sempre estarmos antenados e atualizados com as mudanças tecnológicas do Mundo.</p>
+                    <a href="#" class="more-link">Contratar</a>
+                </div><!-- /.single-offer-style-one -->
+            </div><!-- /.col-lg-3 -->
+        </div><!-- /.row -->
     </div><!-- /.container -->
-</section><!-- /.mission-style-one -->
+</section><!-- /.offer-style-one -->
 @endsection
 
 
