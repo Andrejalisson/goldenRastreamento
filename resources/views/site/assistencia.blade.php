b7a76cdb6744da3759bb035c91b76dc8c064afad
André Jálisson <andrejalisson@gmail.com>
2022-10-07 14:05:30 -0300

    Ajuste titulo assistência

--- resources/views/site/assistencia.blade.php
+++ resources/views/site/assistencia.blade.php
@@ -32,7 +32,7 @@
 <section class="offer-style-one">
     <div class="container">
         <div class="title-block">
-            <h2>Por que escolher a Bidon Golden Service</h2>
+            <h2>Quais são nossas soluções?</h2>
             <span>Somos franqueado da Bidon Corretora de Seguros autorizada pela SUSEP, e por meio dela, temos parceria com mais de 30 seguradoras do país! Permitindo encontrarmos um seguro para seu veículo que atenda as suas necessidades e ao seu bolso!</span><!-- /.tag-line -->
 
         </div><!-- /.title-block -->
