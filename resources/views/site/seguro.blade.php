12146d08566b50bf390e49abd4c9a18935b77b02
André Jálisson <andrejalisson@gmail.com>
2022-10-11 17:45:28 -0300

    4 - substituir o nome contratar na página do seguro por "saiba mais" com link direcionando para o whatsapp

--- resources/views/site/seguro.blade.php
+++ resources/views/site/seguro.blade.php
@@ -22,9 +22,9 @@
                     <div class="icon-block">
                         <i class="fa fa-calculator"></i>
                     </div><!-- /.icon-block -->
-                    <h3><a href="#">Preços  <br> Imbatíveis</a></h3>
+                    <h3><a href="https://wa.me/5585985214611">Preços  <br> Imbatíveis</a></h3>
                     <p>Com a nossa excelente gama de seguradoras e acordos diferenciados, conseguimos preços imbatíveis para você.</p>
-                    <a href="#" class="more-link">Contratar</a>
+                    <a href="https://wa.me/5585985214611" class="more-link">Saiba mais</a>
                 </div><!-- /.single-offer-style-one -->
             </div><!-- /.col-lg-3 -->
             <div class="col-lg-4 col-md-6">
@@ -32,9 +32,9 @@
                     <div class="icon-block">
                         <i class="fa fa-headphones"></i>
                     </div><!-- /.icon-block -->
-                    <h3><a href="#">Atendimento <br> Personalizado</a></h3>
+                    <h3><a href="https://wa.me/5585985214611">Atendimento <br> Personalizado</a></h3>
                     <p>A nossa equipe está pronta para te atender e instruir sempre que você precisar.</p>
-                    <a href="#" class="more-link">Contratar</a>
+                    <a href="https://wa.me/5585985214611" class="more-link">Saiba mais</a>
                 </div><!-- /.single-offer-style-one -->
             </div><!-- /.col-lg-3 -->
             <div class="col-lg-4 col-md-6">
@@ -42,9 +42,9 @@
                     <div class="icon-block">
                         <i class="fa fa-users"></i>
                     </div><!-- /.icon-block -->
-                    <h3><a href="#">Milhares de <br> Clientes</a></h3>
+                    <h3><a href="https://wa.me/5585985214611">Milhares de <br> Clientes</a></h3>
                     <p>Nada melhor do que escolher um serviço que já é aprovado por muitos consumidores.</p>
-                    <a href="#" class="more-link">Contratar</a>
+                    <a href="https://wa.me/5585985214611" class="more-link">Saiba mais</a>
                 </div><!-- /.single-offer-style-one -->
             </div><!-- /.col-lg-3 -->
 
@@ -53,9 +53,9 @@
                     <div class="icon-block">
                         <i class="fa fa-trophy"></i>
                     </div><!-- /.icon-block -->
-                    <h3><a href="#">Estamos presente <br> em todo o País</a></h3>
+                    <h3><a href="https://wa.me/5585985214611">Estamos presente <br> em todo o País</a></h3>
                     <p>Os nossos consultores estão espalhados por todo o território nacional, prontos para te atender.</p>
-                    <a href="#" class="more-link">Contratar</a>
+                    <a href="https://wa.me/5585985214611" class="more-link">Saiba mais</a>
                 </div><!-- /.single-offer-style-one -->
             </div><!-- /.col-lg-3 -->
             <div class="col-lg-4 col-md-6">
@@ -63,9 +63,9 @@
                     <div class="icon-block">
                         <i class="fa fa-handshake-o"></i>
                     </div><!-- /.icon-block -->
-                    <h3><a href="#">Seguradoras</a></h3>
+                    <h3><a href="https://wa.me/5585985214611">Seguradoras</a></h3>
                     <p>Parceria com as melhores seguradoras do mercado. Trabalhamos com as melhores para oferecer o que há de melhor no mercado aos nossos amigos clientes.</p>
-                    <a href="#" class="more-link">Contratar</a>
+                    <a href="https://wa.me/5585985214611" class="more-link">Saiba mais</a>
                 </div><!-- /.single-offer-style-one -->
             </div><!-- /.col-lg-3 -->
             <div class="col-lg-4 col-md-6">
@@ -73,9 +73,9 @@
                     <div class="icon-block">
                         <i class="fa fa-mobile"></i>
                     </div><!-- /.icon-block -->
-                    <h3><a href="#">Tecnologia</a></h3>
+                    <h3><a href="https://wa.me/5585985214611">Tecnologia</a></h3>
                     <p>Tecnologia de última geração para trazer o que há de novo, contemporâneo e usual no mercado de seguros. Trabalhamos para sempre estarmos antenados e atualizados com as mudanças tecnológicas do Mundo.</p>
-                    <a href="#" class="more-link">Contratar</a>
+                    <a href="https://wa.me/5585985214611" class="more-link">Saiba mais</a>
                 </div><!-- /.single-offer-style-one -->
             </div><!-- /.col-lg-3 -->
         </div><!-- /.row -->
